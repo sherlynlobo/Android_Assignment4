4d8c7e861490662ec2134a7add0f031b865311d2
sherlynlobo <sherlynlobo26@gmail.com>
2018-11-18 02:47:39 -0500

    Colleges with Listview added.

---
+++ app/src/main/java/com/example/sherlynlobo/sherlynlobo_mapd711_lab_assignment4/GeorgeBrownCollege.java
@@ -0,0 +1,13 @@
+package com.example.sherlynlobo.sherlynlobo_mapd711_lab_assignment4;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GeorgeBrownCollege extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_george_brown_college);
+    }
+}
---
+++ app/src/main/java/com/example/sherlynlobo/sherlynlobo_mapd711_lab_assignment4/MainActivity.java
@@ -0,0 +1,67 @@
+package com.example.sherlynlobo.sherlynlobo_mapd711_lab_assignment4;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    // Array of strings...
+    String[] collegeArray = {"Centennial College","Seneca College","Humber College","Sheridan College",
+            "Lambton College","George Brown College","Canadore College"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ArrayAdapter adapter = new ArrayAdapter<String>(this,
+                R.layout.activity_campus, collegeArray);
+
+        ListView listView = (ListView) findViewById(R.id.colleges_listview);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                switch (position) {
+                    case 0:
+                        Intent newActivity1 = new Intent(MainActivity.this, CentennialCollege.class);
+                        startActivity(newActivity1);
+                        break;
+                    case 1:
+                        Intent newActivity2 = new Intent(MainActivity.this, SenecaCollege.class);
+                        startActivity(newActivity2);
+                        break;
+                    case 2:
+                        Intent newActivity3 = new Intent(MainActivity.this, HumberCollege.class);
+                        startActivity(newActivity3);
+                        break;
+                    case 3:
+                        Intent newActivity4 = new Intent(MainActivity.this, SheridanCollege.class);
+                        startActivity(newActivity4);
+                        break;
+                    case 4:
+                        Intent newActivity5 = new Intent(MainActivity.this, LambtonCollege.class);
+                        startActivity(newActivity5);
+                        break;
+                    case 5:
+                        Intent newActivity6 = new Intent(MainActivity.this, GeorgeBrownCollege.class);
+                        startActivity(newActivity6);
+                        break;
+                    case 6:
+                        Intent newActivity7 = new Intent(MainActivity.this, CanadoreCollege.class);
+                        startActivity(newActivity7);
+                        break;
+                }
+            }
+
+
+        });
+
+    }
+    }
